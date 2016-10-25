4ea42bcdbff46f100141591fcd011fccc15d6896
Gabe <gabeosx@yahoo.com>
2016-10-25 08:48:05 -0400

    Remove shell scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#Stop if running 
-docker-compose stop
-
-sh ./cleanup.sh
-
-#Remove the existing image
-docker rmi multichain_chain
-docker-compose up -d
--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#Kill all containers
-docker kill `docker ps -aq`
-
-#Remove all exited containers
-#docker rm $(docker ps --all -q -f status=exited)
-docker rm `docker ps -aq`
-
-#Cleanup dead volumes
-docker rmi docker volume rm $(docker volume ls -qf dangling=true)
