0467424453c77cf8d9c7ab23c5a6c4e84a49ef10
Gabe <gabeosx@yahoo.com>
2016-10-26 20:35:13 -0400

    Add RPC port configuration to master node

--- multichain/createEnv.sh
+++ multichain/createEnv.sh
@@ -1,31 +1,13 @@
-### Chain 1 specific conf
-#echo "rpcuser=$RPCUSER" > /opt/chains/chain1/multichain.conf
-#echo "rpcpassword=$RPCPASSWORD" >> /opt/chains/chain1/multichain.conf
+### Create chain
+/usr/local/bin/multichain-util create chain1 -datadir=/opt/chains
 
-### Global conf
-#echo "anyone-can-connect=$ANYONE_CAN_CONNECT" > /opt/chains/multichain.conf
-#echo "anyone-can-send=$ANYONE_CAN_SEND" >> /opt/chains/multichain.conf
-#echo "anyone-can-receive=$ANYONE_CAN_RECEIVE" >> /opt/chains/multichain.conf
-#echo "anyone-can-create=$ANYONE_CAN_CREATE" >> /opt/chains/multichain.conf
-#echo "anyone-can-issue=$ANYONE_CAN_ISSUE" >> /opt/chains/multichain.conf
-#echo "anyone-can-mine=$ANYONE_CAN_MINE" >> /opt/chains/multichain.conf
-#echo "anyone-can-activate=$ANYONE_CAN_ACTIVATE" >> /opt/chains/multichain.conf
-#echo "anyone-can-admin=$ANYONE_CAN_ADMIN" >> /opt/chains/multichain.conf
-
-/usr/local/bin/multichain-util create chain1 -datadir=/opt/chains -default-network-port=$PEER_PORT -default-rpc-port=$RPC_PORT \
-	-anyone-can-connect=$ANYONE_CAN_CONNECT \
-	-anyone-can-send=$ANYONE_CAN_SEND \
-	-anyone-can-receive=$ANYONE_CAN_RECEIVE \
-	-anyone-can-create=$ANYONE_CAN_CREATE \
-	-anyone-can-issue=$ANYONE_CAN_ISSUE \
-	-anyone-can-mine=$ANYONE_CAN_MINE \
-	-anyone-can-activate=$ANYONE_CAN_ACTIVATE \
-	-anyone-can-admin=$ANYONE_CAN_ADMIN
-
+### Configure RPC
 echo "rpcuser=$RPCUSER" > /opt/chains/chain1/multichain.conf
 echo "rpcpassword=$RPCPASSWORD" >> /opt/chains/chain1/multichain.conf
+echo "rpcallowip=$RPC_NETMASK" >> /opt/chains/chain1/multichain.conf
+echo "rpcport=$RPC_PORT" >> /opt/chains/chain1/multichain.conf
 
-
+### Configure params
 sed -i "s/anyone-can-connect = false/anyone-can-connect = $ANYONE_CAN_CONNECT/g" /opt/chains/chain1/params.dat
 sed -i "s/anyone-can-send = false/anyone-can-send = $ANYONE_CAN_SEND/g" /opt/chains/chain1/params.dat
 sed -i "s/anyone-can-receive = false/anyone-can-receive = $ANYONE_CAN_RECEIVE/g" /opt/chains/chain1/params.dat
@@ -35,4 +17,6 @@ sed -i "s/anyone-can-mine = false/anyone-can-mine = $ANYONE_CAN_MINE/g" /opt/cha
 sed -i "s/anyone-can-activate = false/anyone-can-activate = $ANYONE_CAN_ACTIVATE/g" /opt/chains/chain1/params.dat
 sed -i "s/anyone-can-admin = false/anyone-can-admin = $ANYONE_CAN_ADMIN/g" /opt/chains/chain1/params.dat
 
+
+### Start chain
 /usr/local/bin/multichaind chain1 -datadir=/opt/chains start -port=$PEER_PORT -rpcport=$RPC_PORT
